941f303182d0fdf55ca026506d60cd6620dd3876
Caleb Isaac Mora Díaz <calexico659@gmail.com>
2020-08-29 15:03:34 -0500

    Implementacion de Angular material y creacion de modulos [Dashboard, Account, Login, Register, Panel]

---
+++ front-end/src/app/@pages/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'panel',
+    loadChildren: () =>
+      import('./panel/panel.module').then((m) => m.PanelModule),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class DashboardRoutingModule {}
--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountComponent } from './@pages/account/account.component';
+import { DashboardComponent } from './@pages/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'account',
+    component: AccountComponent,
+    loadChildren: () =>
+      import('./@pages/account/account.module').then((m) => m.AccountModule),
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    loadChildren: () =>
+      import('./@pages/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
+  },
+  { path: '**', pathMatch: 'full', redirectTo: 'account' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
